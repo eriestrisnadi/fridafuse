845dc29596e8ebc458e91f479063024c474e2fea
Eries Trisnadi <lognus21@gmail.com>
2024-12-23 16:54:12 +0000

    feat(apk_utils): injection checks

--- src/fridafuse/apk_utils.py
+++ src/fridafuse/apk_utils.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+import re
 from typing import TYPE_CHECKING
 
+from fridafuse import elf_reader
 from fridafuse.constants import ABIS, ARCHITECTURES
 
 if TYPE_CHECKING:
@@ -31,3 +33,31 @@ def get_available_native_libs(lib_dir: Path, arch: str, excludes: list[str] | No
         excludes = []
 
     return [file.name for file in target_dir.iterdir() if file.is_file() and file.name not in excludes]
+
+
+def lib_to_base_name(lib_name: str):
+    return re.sub(r'^(lib)?(.*?)(\.[^.]+)?$', r'\2', lib_name)
+
+
+def is_smali_injected(injection_code: str, smali_file: Path):
+    injection_lines = [line.strip() for line in str(injection_code).strip().splitlines()]
+
+    with smali_file.open('r') as file:
+        smali_content = file.read()
+
+    smali_lines = [line.strip() for line in smali_content.splitlines()]
+
+    return all(line in smali_lines for line in injection_lines)
+
+
+def is_lib_injected(src: Path, target: Path, *, verbose: bool = False):
+    return src.name in [info for (_, _, info) in elf_reader.get_needed(src=target, verbose=verbose)]
+
+
+def is_frida(file: Path):
+    if not file.is_file():
+        return False
+
+    return 'frida' in ' '.join(
+        [info for (tag, _, info) in elf_reader.get_needed(file, verbose=False) if tag == 'SONAME']
+    )
