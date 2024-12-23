7a1fe3d1e621b3d92814e178e3da03939082af93
Eries Trisnadi <lognus21@gmail.com>
2024-12-23 15:31:58 +0000

    refactor: update cache dir

--- src/fridafuse/constants.py
+++ src/fridafuse/constants.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
+from fridafuse.__about__ import __title__
+
 # default values
 GH_BASE_URL = 'https://github.com'
 LATEST_VERSION = 'latest'
-CACHE_DIR = Path.cwd() / '.cache'
+CACHE_DIR = Path.cwd() / f'.{__title__}_cache'
 MANIFEST_NAMESPACE = {'android': 'http://schemas.android.com/apk/res/android'}
 
 # colors
