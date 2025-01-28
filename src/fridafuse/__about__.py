db2a837905c062fb45c9ad07154c4c32fa2ce0f7
Eries Trisnadi <lognus21@gmail.com>
2025-01-28 20:29:55 +0000

    chore: bump version 0.2.0

--- src/fridafuse/__about__.py
+++ src/fridafuse/__about__.py
@@ -3,7 +3,7 @@
 # └  ┴└─┴─┴┘┴ ┴└  └─┘└─┘└─┘
 
 __title__ = 'fridafuse'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __description__ = 'Automatically patch APK with frida-gadget into Smali or Native Library.'
 __readme__ = 'README.md'
 __license__ = {
